105bdf942c099e62e5779b307625a30a1fcf13e3
sahaarnav3 <sahaarnav3@gmail.com>
2025-07-16 15:02:31 +0530

    Completed till Tech Stack section

---
+++ src/components/Connect.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const TechStack = () => {
+  return (
+    <section
+      className="container bg-black text-white "
+      style={{
+        border: "1px solid black",
+        position: "relative",
+        top: "0px",
+      }}
+    >
+      <h1 className="mt-5 pt-5 display-4 fw-semibold">Tech Stack</h1>
+      <p className="text-white-50 fs-4 mb-0">
+        Technologies and tools | work with
+      </p>
+      <span className="border-bottom px-5 border-success border-4"></span>
+      <div></div>
+    </section>
+  );
+};
+
+export default TechStack;
--- src/components/TechStack.jsx
+++ src/components/TechStack.jsx
@@ -3,43 +3,83 @@ import React from "react";
 const TechStack = () => {
   return (
     <section
-      className="container border bg-black text-white"
+      className="container bg-black text-white "
       style={{
-        height: "70vh",
         border: "1px solid black",
         position: "relative",
-        top: "100px",
+        top: "0px",
       }}
     >
-      <h1 className="mt-5 pt-5 display-1 fw-semibold">Arnav Saha</h1>
-      <p className="text-white-50 fs-2 mb-0">full stack dev</p>
+      <h1 className="mt-5 pt-5 display-4 fw-semibold">Tech Stack</h1>
+      <p className="text-white-50 fs-4 mb-0">
+        Technologies and tools | work with
+      </p>
       <span className="border-bottom px-5 border-success border-4"></span>
-      <p
-        className="mt-4"
-        style={{
-          width: "35em",
-          fontSize: "1.3rem",
-          fontWeight: "300",
-          color: "#dbdfe3",
-        }}
-      >
-        Passionate full-stack developer crafting intuitive digital experiences
-        with modern technologies.
-      </p>
-      <p
-        className=""
-        style={{
-          width: "35em",
-          fontSize: "1.3rem",
-          fontWeight: "300",
-          color: "#dbdfe3",
-        }}
-      >
-        Specializing in React, Node.js, and MongoDB to build scalable and user-centered applications.
-      </p>
-      <div className="mt-4">
-        <a href="#contact" className="btn btn-success px-4 me-3" >Contact Me</a>
-        <a className="btn btn-outline-secondary px-4" >Resume</a>
+      <div className="my-5 row justify-content-start">
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://cdn.hugeicons.com/icons/react-solid-standard.svg" alt="react" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}}/>
+          <p className="fs-4 m-0 col-md-10">React</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://cdn.hugeicons.com/icons/bootstrap-solid-rounded.svg" alt="react" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}}/>
+          <p className="fs-4 m-0 col-md-10">Bootstrap</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://cdn.hugeicons.com/icons/tailwindcss-solid-sharp.svg" alt="react" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}}/>
+          <p className="fs-4 m-0 col-md-10">Tailwind CSS</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://cdn.hugeicons.com/icons/java-script-solid-standard.svg" alt="react" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}}/>
+          <p className="fs-4 m-0 col-md-10">Javascript</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/327379/logo-nodejs.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Node.js</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/330398/express.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Express</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/473729/mongodb.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">MongoDB</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/137496/mongoose.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Mongoose</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/508801/dbs-postgresql.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">PostgreSQL</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/378475/vercel-fill.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Vercel</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/424927/vs-code-logo-microsoft.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">VS Code</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/473619/git.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Git</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/512317/github-142.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">GitHub</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/311475/window-dev-tools.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">DevTools</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/488743/accessibility-alt.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Accessibility</p>
+        </div>
+        <div className="col-md-3 col-6 p-0 m-0 py-4 row align-items-center " >
+          <img src="https://www.svgrepo.com/show/477665/meter-1.svg" alt="Logo Nodejs SVG File" width="35" height="35" className="col-sm-2" style={{filter: "invert(1)"}} />
+          <p className="fs-4 m-0 col-md-10">Performance</p>
+        </div>
       </div>
     </section>
   );
